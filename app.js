f4ab439fbbdba15f2419265465f3775768cb17dc
Instafluff <35773245+instafluff@users.noreply.github.com>
2020-01-08 12:00:54 -0800

    API for Sentiment!

--- app.js
+++ app.js
@@ -1,41 +1,48 @@
-var createError = require('http-errors');
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
+const fs = require( "fs" );
+const unfluff = require( "unfluff" );
+let words = fs.readFileSync( "AFINN-111.txt" ).toString().split( "\n" );
+let wordsWithFriends = {};
+for( var i = 0, len = words.length; i < len; i++ ) {
+	let parts = words[ i ].split('\t');
+	wordsWithFriends[ parts[ 0 ] ] = parseInt( parts[ 1 ] );
+}
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+const SentientWeb = require( "webwebweb" );
+SentientWeb.APIs[ "/" ] = async ( qs, body ) => {
+	if( qs.url ) {
+		let sentiment = await calculateSentiment( qs.url );
+		return {
+			url: qs.url,
+			score: sentiment.score,
+			vader: sentiment.vader,
+			text: sentiment.text,
+		};
+	}
+	return {
+		error: "Missing URL",
+	};
+};
+SentientWeb.Run( 8001 );
 
-var app = express();
-
-// view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'twig');
-
-app.use(logger('dev'));
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
-
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
-
-// catch 404 and forward to error handler
-app.use(function(req, res, next) {
-  next(createError(404));
-});
-
-// error handler
-app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
-
-  // render the error page
-  res.status(err.status || 500);
-  res.render('error');
-});
-
-module.exports = app;
+const fetch = require( "node-fetch" );
+const vader = require('vader-sentiment');
+async function calculateSentiment( url ) {
+	let body = await fetch( url ).then( r => r.text() );
+	var content = unfluff( body );
+	var allTheWords = content[ "text" ].split( ' ' );
+	let totalScore = 0, totalCount = 0;
+	for( var i = 0, len = allTheWords.length; i < len; i++ ) {
+		var currentWord = allTheWords[ i ];
+		if( wordsWithFriends[ currentWord ] ) {
+			totalScore += wordsWithFriends[ currentWord ];
+			totalCount++;
+		}
+	}
+	const intensity = vader.SentimentIntensityAnalyzer.polarity_scores( content[ "text" ] );
+	console.log( intensity );
+	return {
+		text: content["text"],
+		vader: intensity,
+		score: ( totalScore / totalCount + 5 ) / 10,
+	};
+}
